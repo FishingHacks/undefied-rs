6f0fa29b443672f6e3f55ca8f9a22ac04f1423c0
FishingHacks <fishinghacks@proton.me>
2024-01-19 10:46:35 +0100

    more std

--- _.asm
+++ _.asm
@@ -7,36 +7,188 @@ _start:
     ;; -- call main --
     mov rax, rsp
     mov rsp, [ret_stack_rsp]
-    call addr_20
+    call addr_8
     mov [ret_stack_rsp], rsp
     mov rsp, rax
     ;; -- exit --
     mov rax, 60
     mov rdi, 0
     syscall
-    ;; -- fn_skip(main) --
-    jmp addr_54
-    ;; -- fn(main) --
-addr_20:
+    ;; -- fn_skip(fputs) --
+    jmp addr_9
+    ;; -- fn(fputs) --
+addr_0:
     sub rsp, 0
     mov [ret_stack_rsp], rsp
     mov rsp, rax
+    ;; -- int --
+    mov rax, 1
+    push rax
+    ;; -- syscall3 --
+    pop rax
+    pop rdi
+    pop rsi
+    pop rdx
+    syscall
+    push rax
     ;; end/ret
     mov rax, rsp
     mov rsp, [ret_stack_rsp]
     add rsp, 0
     ret
-    addr_54:
+    addr_9:
+    ;; -- fn_skip(puts) --
+    jmp addr_10
+    ;; -- fn(puts) --
+addr_1:
+    sub rsp, 0
+    mov [ret_stack_rsp], rsp
+    mov rsp, rax
+    ;; -- int --
+    mov rax, 1
+    push rax
+    ;; -- call fn --
+    mov rax, rsp
+    mov rsp, [ret_stack_rsp]
+    call addr_0
+    mov [ret_stack_rsp], rsp
+    mov rsp, rax
+    ;; -- drop --
+    pop rax
+    ;; end/ret
+    mov rax, rsp
+    mov rsp, [ret_stack_rsp]
+    add rsp, 0
+    ret
+    addr_10:
+    ;; -- fn_skip(!bool) --
+    jmp addr_13
+    ;; -- fn(!bool) --
+addr_4:
+    sub rsp, 0
+    mov [ret_stack_rsp], rsp
+    mov rsp, rax
+
+    ;; -- !8 --
+    ;; !
+    pop rax
+    pop rbx
+    mov [rax], bl
+    ;; end/ret
+    mov rax, rsp
+    mov rsp, [ret_stack_rsp]
+    add rsp, 0
+    ret
+    addr_13:
+    ;; -- fn_skip(@bool) --
+    jmp addr_14
+    ;; -- fn(@bool) --
+addr_5:
+    sub rsp, 0
+    mov [ret_stack_rsp], rsp
+    mov rsp, rax
+    ;; -- @8 --
+    ;; @
+    pop rax
+    xor rbx, rbx
+    mov bl, [rax]
+    push rbx
+    ;; -- cast(bool) --
+    ;; end/ret
+    mov rax, rsp
+    mov rsp, [ret_stack_rsp]
+    add rsp, 0
+    ret
+    addr_14:
+    ;; -- fn_skip(Bool::to_string) --
+    jmp addr_22
+    ;; -- fn(Bool::to_string) --
+addr_7:
+    sub rsp, 0
+    mov [ret_stack_rsp], rsp
+    mov rsp, rax
+    ;; -- if --
+    pop rax
+    cmp rax, 0
+    je addr_20__
+    ;; -- str --
+    mov rax, 4
+    push rax
+    push str_3
+    ;; -- else --
+addr_20:
+    jmp addr_21
+addr_20__:
+    ;; -- str --
+    mov rax, 5
+    push rax
+    push str_4
+    ;; -- end --
+addr_21:
+    ;; end/ret
+    mov rax, rsp
+    mov rsp, [ret_stack_rsp]
+    add rsp, 0
+    ret
+    addr_22:
+    ;; -- fn_skip(main) --
+    jmp addr_23
+    ;; -- fn(main) --
+addr_8:
+    sub rsp, 1
+    mov [ret_stack_rsp], rsp
+    mov rsp, rax
+    ;; -- constant --
+    mov rax, 1
+    push rax
+    ;; -- push_local_mem --
+    mov rax, [ret_stack_rsp]
+    add rax, 0
+    push rax
+    ;; -- call fn --
+    mov rax, rsp
+    mov rsp, [ret_stack_rsp]
+    call addr_4
+    mov [ret_stack_rsp], rsp
+    mov rsp, rax
+    ;; -- push_local_mem --
+    mov rax, [ret_stack_rsp]
+    add rax, 0
+    push rax
+    ;; -- call fn --
+    mov rax, rsp
+    mov rsp, [ret_stack_rsp]
+    call addr_5
+    mov [ret_stack_rsp], rsp
+    mov rsp, rax
+    ;; -- call fn --
+    mov rax, rsp
+    mov rsp, [ret_stack_rsp]
+    call addr_7
+    mov [ret_stack_rsp], rsp
+    mov rsp, rax
+    ;; -- call fn --
+    mov rax, rsp
+    mov rsp, [ret_stack_rsp]
+    call addr_1
+    mov [ret_stack_rsp], rsp
+    mov rsp, rax
+    ;; end/ret
+    mov rax, rsp
+    mov rsp, [ret_stack_rsp]
+    add rsp, 1
+    ret
+    addr_23:
     mov rax, 60
     mov rdi, 0
     syscall
 
 segment .data
     str_0: db 10
-    str_1: db 45
-    str_2: db 80, 65, 78, 73, 67, 58, 32
-    str_3: db 69, 82, 82, 79, 82, 58, 32, 103, 101, 116, 95, 101, 108, 101, 109, 101, 110, 116, 32, 99, 97, 108, 108, 101, 100, 32, 119, 105, 116, 104, 32, 105, 110, 100, 101, 120, 32, 62, 61, 32, 108, 101, 110
-    str_4: db 69, 82, 82, 79, 82, 58, 32, 114, 101, 109, 111, 118, 101, 95, 101, 108, 101, 109, 101, 110, 116, 32, 99, 97, 108, 108, 101, 100, 32, 119, 105, 116, 104, 32, 110, 111, 32, 101, 108, 101, 109, 101, 110, 116, 115
+    str_1: db 116, 114, 117, 101, 10
+    str_2: db 102, 97, 108, 115, 101, 10
+    str_3: db 116, 114, 117, 101
+    str_4: db 102, 97, 108, 115, 101
 
 segment .bss
     ret_stack_rsp: resq 1
